aec9aa096255279682fc1da70bddbaeb5eb6c150
Daniel Orejuela <dorejuela@externos.itnow.es>
2021-10-26 23:53:22 +0200

    20211026_235313

---
+++ users/confirmsignup.py
@@ -0,0 +1,37 @@
+import json
+import boto3
+import os
+
+
+def handler(event, context):
+    code = 200
+    body = {}
+    if event['body'] is None:
+        code = 400
+    else:
+        activator = json.loads(event['body'])
+        activator_code = activator.get['code', '']
+        email = activator.get['username', '']
+
+        client = boto3.client('cognito-idp')
+        try:
+            result = client.confirm_sign_up(
+                ClientId=os.environ['CLIENT_ID'],
+                Username=email, ConfirmationCode=activator_code)
+            print(result)
+            body = {
+                "message": "Usuario activado"
+            }
+        except Exception as e:
+            code = 400
+            body = {
+                "message": e.args[0]
+            }
+    return {
+        'statusCode': code,
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS, POST'
+        },
+        'body': json.dumps(body)
+    }
---
+++ users/login.py
@@ -0,0 +1,39 @@
+import json
+import boto3
+import os
+
+
+def handler(event, context):
+    code = 200
+    body = {}
+    if event['body'] is None:
+        code = 400
+    else:
+        user = json.loads(event['body'])
+        email = user.get['username', '']
+        password = user.get['password', '']
+
+        client = boto3.client('cognito-idp')
+        ap = {
+            'USERNAME': email,
+            'PASSWORD': password
+        }
+        try:
+            result = client.iniciate_auth(AuthFlow='USER_PASSWORD_AUTH', AuthPatameters=ap,
+                                          ClientId=os.environ['CLIENT_ID'])
+            body = {
+                "token": result['AuthenticationResult']['IdToken']
+            }
+        except Exception as e:
+            code = 400
+            body = {
+                "message": e.args[0]
+            }
+    return {
+        'statusCode': code,
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS, POST'
+        },
+        'body': json.dumps(body)
+    }
